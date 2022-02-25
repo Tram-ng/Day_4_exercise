1ffacf20822a1162b84ab3463d1bdac7f0e7dd5f
Tracie Nguyen <91838259+Tram-ng@users.noreply.github.com>
2022-02-25 08:56:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+
+import random
+#import  my_module
+
+random_integer = random.randint(1,10)
+print  (random_integer)
+
+
+#print (my_module.pi)
+
+
+
+random_float  = random.random() #only generate between 0 and 1
+print (random_float)
+
+  
+#how to create a float random number from 0 to 5? Multiple 5  with the .random(),then number wont include 5
+
